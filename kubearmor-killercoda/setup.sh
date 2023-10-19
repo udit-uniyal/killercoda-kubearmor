68b18a88166746c39352edbf2bfd9f1d52af0ceb
Udit Uniyal <115368361+udit-uniyal@users.noreply.github.com>
2023-10-19 06:01:10 +0530

    Update setup.sh

--- kubearmor-killercoda/setup.sh
+++ kubearmor-killercoda/setup.sh
@@ -1,3 +1,17 @@
-curl -sfL http://get.kubearmor.io/ | sudo sh -s -- -b /usr/local/bin && karmor install
-
+curl -sfL http://get.kubearmor.io/ | sudo sh -s -- -b /usr/local/bin && karmor install && kubectl create deployment nginx --image=nginx && POD=$(kubectl get pod -l app=nginx -o name) && cat <<EOF | kubectl apply -f -
+apiVersion: security.kubearmor.com/v1
+kind: KubeArmorPolicy
+metadata:
+  name: block-pkg-mgmt-tools-exec
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  process:
+    matchPaths:
+    - path: /usr/bin/apt
+    - path: /usr/bin/apt-get
+  action:
+    Block
+EOF
 
