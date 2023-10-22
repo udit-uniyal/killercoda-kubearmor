f15cb3aad29de7429583c996ce1aaa25a7fbc289
Udit Uniyal <115368361+udit-uniyal@users.noreply.github.com>
2023-10-22 11:31:49 +0530

    Update setup.sh

--- kubearmor-killercoda/setup.sh
+++ kubearmor-killercoda/setup.sh
@@ -1,15 +1,30 @@
-echo "apiVersion: security.kubearmor.com/v1
-kind: KubeArmorPolicy
-metadata:
-  name: block-pkg-mgmt-tools-exec
-spec:
-  selector:
-    matchLabels:
-      app: nginx
-  process:
-    matchPaths:
-    - path: /usr/bin/apt
-    - path: /usr/bin/apt-get
-  action:
-    Block" >> Deny-execution-of-package-management-tool.yaml
+#!/bin/bash
+
+namespace="kubearmor"
+duration=120  # Watch duration in seconds
+
+# Start watching the pods
+watch -n 1 "kubectl get po -n $namespace"
+
+# Wait for the specified duration
+sleep $duration
+
+# Check if all pods are in the "Running" state
+while true; do
+  all_running=true
+  while read -r line; do
+    if [[ ! $line =~ "Running" ]]; then
+      all_running=false
+      break
+    fi
+  done < <(kubectl get po -n $namespace | awk 'NR > 1 {print $3}')
+  
+  if $all_running; then
+    echo "All pods are in the 'Running' state. Exiting..."
+    break
+  else
+    echo "Not all pods are in the 'Running' state. Continuing to check..."
+    sleep 5  # Check every 5 seconds
+  fi
+done
 
