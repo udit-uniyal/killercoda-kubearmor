146c1de0838bd5dba8e114d19b7cc401ea0c3b13
Udit Uniyal <115368361+udit-uniyal@users.noreply.github.com>
2023-10-23 20:51:36 +0530

    Update setup.sh

--- kubearmor-killercoda/setup.sh
+++ kubearmor-killercoda/setup.sh
@@ -1,19 +1,19 @@
-cat > setup.sh << EOL
+cat > setup.sh << 'EOL'
 #!/bin/bash
 
 namespace="kubearmor"
 duration=256  # Watch duration in seconds
 
 # Start watching the pods in the background
-watch -n 1 "kubectl get po -n kubearmor" &
+watch -n 1 "kubectl get po -n $namespace" &
 watch_pid=$!
 
 # Wait for the specified duration
-sleep 256
+sleep $duration
 
 # Check if all pods are in the "Running" state
 while true; do
-  if kubectl get po -n kubearmor | awk 'NR > 1 {print $3}' | grep -q -v "Running"; then
+  if kubectl get po -n $namespace | awk 'NR > 1 {print $3}' | grep -q -v "Running"; then
     echo "Some pods aren't 'Running'; ongoing verification..."
     sleep 15  # Check every 15 seconds
   else
@@ -24,6 +24,8 @@ done
 
 # Terminate the watch process
 exit
-EOL 
+EOL
+
+
 
 
