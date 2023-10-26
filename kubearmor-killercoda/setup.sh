588d634f8a44ab731498c2456ee39987c00cd113
Udit Uniyal <115368361+udit-uniyal@users.noreply.github.com>
2023-10-26 19:12:37 +0530

    Update setup.sh

--- kubearmor-killercoda/setup.sh
+++ kubearmor-killercoda/setup.sh
@@ -1,22 +1,15 @@
 cat > setup.sh << 'EOL'
 #!/bin/bash
-
 namespace="kubearmor"
 duration=120
-
 watch -n 1 "kubectl get po -n $namespace" &
 watch_pid=$!
-
 sleep $duration
-
 while kubectl get po -n $namespace | tail -n +2 | awk '{print $3}' | grep -q -v "Running"; do
   sleep 8
 done
-
 kill $watch_pid
-
 echo "All pods are in the 'Running' state. Exiting..."
-
 EOL
 
 
