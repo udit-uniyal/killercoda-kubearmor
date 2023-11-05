2e3dfffc9b9361ce07dae6eae2c981a9058f0b61
Udit Uniyal <115368361+udit-uniyal@users.noreply.github.com>
2023-11-05 09:40:29 +0530

    Update KillerCoda_K3s_Installation.sh

--- kubearmor-killercoda-Testing/KillerCoda_K3s_Installation.sh
+++ kubearmor-killercoda-Testing/KillerCoda_K3s_Installation.sh
@@ -5,32 +5,33 @@ if [ "$RUNTIME" == "" ]; then
         RUNTIME="docker"
     elif [ -S /var/run/crio/crio.sock ]; then
         RUNTIME="crio"
-    else # default
+    else
+        # Default runtime
         RUNTIME="containerd"
     fi
 fi
 
-# create a single-node K3s cluster
-if [ "$RUNTIME" == "docker" ]; then # docker
+# Create a single-node K3s cluster based on the chosen runtime
+if [ "$RUNTIME" == "docker" ]; then
     CGROUP_SYSTEMD=$(docker info 2> /dev/null | grep -i cgroup | grep systemd | wc -l)
     if [ $CGROUP_SYSTEMD == 1 ]; then
-        curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.23.9+k3s1" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik --docker --kubelet-arg cgroup-driver=systemd" sh -
+        curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.23.9+k3s1" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik --docker --kubelet-arg cgroup-driver=systemd" sh - 2>/dev/null
         [[ $? != 0 ]] && echo "Failed to install k3s" && exit 1
-    else # cgroupfs
-        curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.23.9+k3s1" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik --docker" sh -
+    else
+        curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.23.9+k3s1" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik --docker" sh - 2>/dev/null
         [[ $? != 0 ]] && echo "Failed to install k3s" && exit 1
     fi
-elif [ "$RUNTIME" == "crio" ]; then # cri-o
-  curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik --container-runtime-endpoint unix:///var/run/crio/crio.sock --kubelet-arg cgroup-driver=systemd" sh -
+elif [ "$RUNTIME" == "crio" ]; then
+  curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik --container-runtime-endpoint unix:///var/run/crio/crio.sock --kubelet-arg cgroup-driver=systemd" sh - 2>/dev/null
   [[ $? != 0 ]] && echo "Failed to install k3s" && exit 1
-else # use containerd by default
-  curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik" sh -
+else
+  curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik" sh - 2>/dev/null
   [[ $? != 0 ]] && echo "Failed to install k3s" && exit 1
 fi
 
 if [[ $(hostname) = kubearmor-dev* ]]; then
     mkdir -p /home/vagrant/.kube
-    sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config 2>/dev/null
+    sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
     echo "export KUBECONFIG=/home/vagrant/.kube/config" | tee -a /home/vagrant/.bashrc
     PATH=$PATH:/bin:/usr/bin:/usr/local/bin
 else
@@ -41,28 +42,17 @@ else
         echo "Found $KUBECONFIG already in place ... backing it up to $KUBECONFIG.backup"
         cp $KUBECONFIG $KUBECONFIG.backup
     fi
-    sudo cp /etc/rancher/k3s/k3s.yaml $KUBECONFIG 2>/dev/null
+    sudo cp /etc/rancher/k3s/k3s.yaml $KUBECONFIG
     echo "export KUBECONFIG=$KUBECONFIG" | tee -a ~/.bashrc
 fi
 
-echo "Wait for initialization"
-sleep 15
-
-# Loop until all pods are in the "Running" state or the timeout is reached
-timeout="15m"
-endtime=$(date -ud "$timeout" +%s)
-
-while [[ $(date -u +%s) -le $endtime ]]; do
-    running_pods=$(kubectl get pods -A --field-selector=status.phase=Running --no-headers | wc -l)
-    total_pods=$(kubectl get pods -A --no-headers | wc -l)
-
-    if [ "$running_pods" -eq "$total_pods" ]; then
-        echo "All pods are in the 'Running' state."
+while true; do
+    status=$(kubectl get pods -A -o jsonpath={.items[*].status.phase})
+    if [[ $(echo $status | tr ' ' '\n' | grep -c -v "Running") -eq 0 ]]; then
         break
-    else
-        echo "Waiting for pods to be in the 'Running' state..."
-        sleep 1
     fi
+    echo "Waiting for all pods to be in the 'Running' state"
+    sleep 1
 done
 
 kubectl get pods -A
